5e84d5eb47aa9a8aa165f03e4f2f83a5e08eb412
Stuart 'Stevie' Leitch <hotblack@disasterarea.org.uk>
2016-04-17 23:18:34 +0100

    Updated for spanners version 3.5

--- spanners-nodejs/server.js
+++ spanners-nodejs/server.js
@@ -10,7 +10,7 @@ var server = app.listen(8081, function () {
   console.log("Websocket event broadcaster REST API listening on http://%s:%s", host, port)
 });
 
-
+// Broadcast updates to all WebSocketServer clients
 app.post('/notify/spanner/:spanner_id/update', function (req, res) {
    var spanner_id = req.params.spanner_id;
    console.log('Event: spanner %s updated', spanner_id);
